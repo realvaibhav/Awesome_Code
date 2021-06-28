2815342965e84b816c455768cda0d3690a823d1d
realvaibhav <realvaibhav2002@gmail.com>
2021-06-28 23:03:49 +0530

    Sum of two numbers without using arithmetic operators

---
+++ half_adder.cpp
@@ -0,0 +1,36 @@
+//Initial Template for C++
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    int sum(int a , int b)
+    {
+        int c=(a^b) ;  // sum
+        int d=(a&b) ;  // carry 
+        int sum=c + 2*d ; // our answer
+        return sum ;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a,b;
+        cin >> a>>b;
+        
+        Solution ob;
+        cout<< ob.sum(a,b) <<"\n";
+        
+    }
+    return 0;
+}
+  // } Driver Code Ends
