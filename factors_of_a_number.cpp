641830523f88f953d2fa8d88be2a470c422c3886
realvaibhav <realvaibhav2002@gmail.com>
2021-07-29 15:31:40 +0530

    all factors of a number in O(√n)

---
+++ factors_of_a_number.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll ;
+
+int main()
+{  
+    int n ;
+    cin >> n ;
+    int num1=0 , num2=0 ;
+    for(int i=1 ; i<=sqrt(n) ; i++) // time complexity is just O(√n)
+    {
+        if(n%i==0)
+        {
+            num1=i   ;
+            num2=n/i ;
+        
+            if(num1!=num2)
+            {
+                cout << num1 << " " << num2 << endl ;
+            }
+            else  // n is a perfect square
+            {
+                cout << num1 << endl ;
+            }
+        }
+    }
+
+
+    return 0  ;
+}
