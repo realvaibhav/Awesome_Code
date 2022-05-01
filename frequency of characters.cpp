544ee1624f4e377e91d28db1c9a0fb5e80a78673
realvaibhav <realvaibhav2002@gmail.com>
2022-05-01 18:03:14 +0530

    frequency of characters in a string

---
+++ frequency of characters.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s ;
+    getline(cin , s)  ;
+    int ans[256]={0}  ;   // intialize with 0
+
+    for(int i=0 ; i<s.size() ; i++)
+    {
+        ans[s[i]]++  ;
+    }
+    for(int i=0 ; i<256 ; i++)
+    {
+        if(ans[i]!=0)
+        {
+            cout << "frequency of " << (char)i << " : " << ans[i] << endl ;
+        }
+    }
+    
+     
+    return 0  ;
+}
